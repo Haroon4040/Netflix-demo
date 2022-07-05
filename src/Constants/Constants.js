e295e19ba6260f24022dbf62003e34a852dab6b4
Haroon Habeeb <haroon4040@gmail.com>
2022-07-05 13:41:34 +0530

    Final file

---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '48d088138370bb23b8c6189277f82c8a'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
---
+++ src/Urls.jsx
@@ -0,0 +1,9 @@
+import {API_KEY} from './Constants/Constants'
+
+export const originals=`discover/tv?api_key=${API_KEY}&with_networks=213`
+
+export const action=`discover/movie?api_key=${API_KEY}&with_genres=28`
+
+export const thriller=`discover/movie?api_key=${API_KEY}&with_genres=18`
+
+export const animation=`discover/tv?api_key=${API_KEY}&with_networks=35`
